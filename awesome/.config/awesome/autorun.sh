117652871702b5d5f62dd08c0870b5052fc1aed2
Bilal Saad <bilalsaad923@gmail.com>
2023-08-19 16:20:02 -0700

    remove autorun useless sleep

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -7,8 +7,6 @@ function run {
   fi
 }
 
-sleep 3
-
 # dual monitor, vertical 4k left 125%, main horiztonal 1440p
 if ! xrandr | grep -q '4180 x 2880' ; then
   xrandr --output DVI-D-0 --off --output HDMI-0 --mode 3840x2160 --scale 0.75x0.75 --pos 0x0 --rotate left --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --rate 165 --pos 1620x720 --rotate normal --output DP-5 --off --output HDMI-A-1-0 --off --output DisplayPort-1-0 --off --output DisplayPort-1-1 --off --output DisplayPort-1-2 --off
