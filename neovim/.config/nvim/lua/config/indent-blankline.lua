b83ba4a7aca8e81785911441f48ec553c8dee932
Bilal Saad <bilalsaad923@gmail.com>
2023-10-13 22:31:04 -0700

    update indent-blankline config

--- neovim/.config/nvim/lua/config/indent-blankline.lua
+++ neovim/.config/nvim/lua/config/indent-blankline.lua
@@ -1,23 +1,22 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
 	return
 end
 
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-	"help",
-	"startify",
-	"dashboard",
-	"packer",
-	"neogitstatus",
-	"NvimTree",
-	"Trouble",
-}
-
-indent_blankline.setup({
-  show_trailing_blankline_indent = false,
-  use_treesitter = true,
-  char = "▏",
-  context_char = "▏",
-  show_current_context = true,
+ibl.setup({
+ exclude = {
+    buftypes = {
+      "terminal",
+      "nofile",
+    },
+    filetypes = {
+      "help",
+      "startify",
+      "dashboard",
+      "packer",
+      "neogitstatus",
+      "NvimTree",
+      "Trouble",
+    }
+  },
 })
